f20725b98a7b6fdb7eeb65a76b8a057b05de424f
Thanaphon Sarnsamuth <thanaphon.sar@axonstech.com>
2025-11-29 16:58:03 +0700

    add for-in and for-of loop examples for color array

---
+++ src/for-in.ts
@@ -0,0 +1,4 @@
+let color: string[] = ["red", "blue", "green", "yellow", "orange", "purple"];
+for (const index in color) {
+  console.log(`Index: ${index}, Value: ${color[index]}`);
+}
---
+++ src/for-of.ts
@@ -0,0 +1,4 @@
+let color: string[] = ["red", "blue", "green", "yellow", "orange", "purple"];
+for (const c of color) {
+  console.log(c);
+}
