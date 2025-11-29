13dc16749ccd08745dafeea61ef3b55d923acc6d
Thanaphon Sarnsamuth <thanaphon.sar@axonstech.com>
2025-11-29 16:38:44 +0700

    add logic cal student score from task 8.9

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-let x = 5;
-let y = 13;
-if (x > 5 && y > 12) {
-  console.log("ค่ามากจัง");
+// let x = 11;
+// if (x > 10) {
+//   console.log("x is more than 10");
+// } else if (x < 5) {
+//   console.log("x is less than 5");
+// } else {
+//   console.log("x is between 5 and 10");
+// }
+
+const studentScore = 81;
+if (studentScore > 80) {
+  console.log("A");
+} else if (studentScore > 60) {
+  console.log("B");
+} else if (studentScore > 50) {
+  console.log("C");
 } else {
-  console.log(x * y + x / y);
+  console.log("F");
 }
