a6d777484a74d8404f059dad9513b86ba415358f
Thanaphon Sarnsamuth <thanaphon.sar@axonstech.com>
2025-11-29 16:31:10 +0700

    add condition for checking x and y

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,7 @@
-console.log("Hello, TypeScript with Node.js!");
-
-let x: number | string = 10;
-x = "hello";
-if (typeof x === "string") {
-  console.log("x is a string");
-} else if (typeof x === "number") {
-  console.log("x is a number");
+let x = 5;
+let y = 13;
+if (x > 5 && y > 12) {
+  console.log("ค่ามากจัง");
 } else {
-  console.log("x is neither a string nor a number");
+  console.log(x * y + x / y);
 }
-
-let i = 0;
-i = "hello" as any;
-console.log(i);
-
-let b = 10;
-b = 15;
-console.log(b);
-
-// const a = 10;
-// a = 15;
-// console.log(a);
-
-interface Person {
-  name: string;
-  age: number;
-}
-
-// const a: Person = {
-//   name: "Alice",
-//   age: 30,
-// };
-
-// a = {
-//   name: "Bob",
-//   age: 25,
-// };
-// console.log(a);
-
-let a: Person = {
-  name: "Alice",
-  age: 30,
-};
-
-a.name = "Bob";
-a.age = 25;
-console.log(a);
-
-const list = [1, 2, 3, 4, 5];
-console.log(list);
